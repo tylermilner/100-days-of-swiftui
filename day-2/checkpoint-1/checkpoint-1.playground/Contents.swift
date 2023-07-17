6995fe666780a2902399ffd17ccea4c7151b0b79
Tyler Milner <tylermilner@gmail.com>
2023-07-17 00:02:13 -0500

    Checkpoints for days 1-6.

---
+++ day-2/checkpoint-1/checkpoint-1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let tempInCelsius = 30.0
+
+let tempInFahrenheit = ((tempInCelsius * 9) / 5) + 32
+
+print("\(tempInCelsius)° in Celsius is \(tempInFahrenheit)° in Fahrenheit")
---
+++ day-4/checkpoint-2/checkpoint-2.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let strings = ["1", "2", "1"]
+
+let numberOfItems = strings.count
+print("\(numberOfItems) items")
+
+let stringsSet = Set(strings)
+print("\(stringsSet.count) unique items")
