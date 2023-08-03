a2415da8b074e887a898fcc70c86f06dc7be9e93
Tyler Milner <tylermilner@gmail.com>
2023-08-02 22:57:53 -0500

    Day 22 - challenge 1 - keep score.

--- day-20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ day-20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var currentScore = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -57,7 +58,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(currentScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -68,15 +69,17 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(currentScore)")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            currentScore += 1
         } else {
             scoreTitle = "Wrong"
+            currentScore -= 1
         }
         
         showingScore = true
