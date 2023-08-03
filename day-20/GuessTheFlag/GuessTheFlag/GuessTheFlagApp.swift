d7e66cf9c50f6479eaee11a4b2198d8eb40d64ac
Tyler Milner <tylermilner@gmail.com>
2023-08-02 22:45:08 -0500

    Day 21 complete.

---
+++ day-20/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tyler Milner on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
