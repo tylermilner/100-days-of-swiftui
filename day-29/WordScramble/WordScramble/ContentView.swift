c52b01525d05b6e0a289721344c5d2cb4ca0e84e
Tyler Milner <tylermilner@gmail.com>
2023-08-08 23:02:13 -0500

    Day 30 complete.

--- day-29/WordScramble/WordScramble/ContentView.swift
+++ day-29/WordScramble/WordScramble/ContentView.swift
@@ -7,58 +7,110 @@
 
 import SwiftUI
 
-// Working with strings
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
-        Text("")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+        }
     }
     
-    func test() {
-        let word = "swift"
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        
         let range = NSRange(location: 0, length: word.utf16.count)
-        
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        let allGood = misspelledRange.location == NSNotFound
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
-
-// Loading resources from your app bundle
-//struct ContentView: View {
-//
-//    var body: some View {
-//        Text("")
-//    }
-//
-//    func loadFile() {
-//        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-//            if let fileContents = try? String(contentsOf: fileURL) {
-//                // file loaded
-//            }
-//        }
-//    }
-//}
-
-// Introducing List, your best friend
-//struct ContentView: View {
-//    let people = ["Finn", "Leia", "Luke", "Rey"]
-//
-//    var body: some View {
-//        List {
-//            Text("Static row")
-//
-//            ForEach(people, id: \.self) {
-//                Text($0)
-//            }
-//
-//            Text("Static row")
-//        }
-//        .listStyle(.grouped)
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
