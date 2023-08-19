bdbcd91affdd417a4d420ddea59a27bcc1dc44de
Tyler Milner <tylermilner@gmail.com>
2023-08-19 16:17:34 -0500

    Day 32 complete.

---
+++ day-32/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tyler Milner on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ day-32/Animations/Animations/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Tyler Milner on 8/15/23.
+//
+
+import SwiftUI
+
+// Creating explicit animations
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+//// Animating bindings
+//struct ContentView: View {
+//    @State private var animationAmount = 1.0
+//
+//    var body: some View {
+//        print(animationAmount)
+//
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                    .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundColor(.white)
+//            .clipShape(Circle())
+//            .scaleEffect(animationAmount)
+//        }
+//    }
+//}
+
+//// Customizing animations in SwiftUI
+//struct ContentView: View {
+//    @State private var animationAmount = 1.0
+//
+//    var body: some View {
+//        Button("Tap Me") {
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .overlay(
+//            Circle()
+//                .stroke(.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(2 - animationAmount)
+//                .animation(
+//                    .easeInOut(duration: 1)
+//                    .repeatForever(autoreverses: false),
+//                    value: animationAmount
+//                )
+//        )
+//        .onAppear {
+//            animationAmount = 2
+//        }
+//    }
+//}
+
+//// Creating implicit animations
+//struct ContentView: View {
+//    @State private var animationAmount = 1.0
+//
+//    var body: some View {
+//        Button("Tap Me") {
+//            animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.default, value: animationAmount)
+//    }
+//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
