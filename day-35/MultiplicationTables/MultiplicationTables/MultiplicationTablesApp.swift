bdb9e15fc8138a304994f719ba8e8226b69bf8c8
Tyler Milner <tylermilner@gmail.com>
2023-08-20 13:06:12 -0500

    Day 35 - blank project created.

---
+++ day-35/MultiplicationTables/MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Tyler Milner on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
