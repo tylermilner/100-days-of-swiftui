b92e7d017bde3a367c79afbaeecec4a286c291a3
Tyler Milner <tylermilner@gmail.com>
2023-08-20 22:17:16 -0500

    Day 35 WIP. Basic game logic working.

--- day-35/MultiplicationTables/MultiplicationTables/ContentView.swift
+++ day-35/MultiplicationTables/MultiplicationTables/ContentView.swift
@@ -11,8 +11,16 @@ struct MultiplicationQuestion {
     let firstNumber: Int
     let secondNumber: Int
     
+    var question: String {
+        "What is \(firstNumber) x \(secondNumber)?"
+    }
+    
     var answer: Int {
         firstNumber * secondNumber
+    }
+    
+    var answerDescription: String {
+        "\(firstNumber) x \(secondNumber) = \(answer)"
     }
 }
 
@@ -23,6 +31,8 @@ extension MultiplicationQuestion: Identifiable {
 }
 
 struct ContentView: View {
+    @State private var showingSettings = true
+    
     private static let minimumTableOption = 2
     private static let maximumTableOption = 12
     @State private var maximumTable = minimumTableOption
@@ -31,34 +41,60 @@ struct ContentView: View {
     @State private var numberOfQuestions = numberOfQuestionsOptions[0]
     
     @State private var questions: [MultiplicationQuestion] = []
+    @State private var currentQuestion = 0
+    
+    @State private var answer = ""
+    @State private var correctAnswers = 0
+    @State private var resultTitle = ""
+    @State private var resultMessage = ""
+    @State private var showingResult = false
+    @State private var showingFinalScore = false
     
     var body: some View {
-        NavigationView {
-            Form {
-                Section("Practice") {
-                    Stepper("Tables up to...\(maximumTable)", value: $maximumTable, in: ContentView.minimumTableOption...ContentView.maximumTableOption)
-                    
-                    Picker("Number of questions", selection: $numberOfQuestions) {
-                        ForEach(ContentView.numberOfQuestionsOptions, id: \.self) {
-                            Text("\($0) questions")
+        if showingSettings {
+            NavigationView {
+                Form {
+                    Section("Settings") {
+                        Stepper("Tables up to...\(maximumTable)", value: $maximumTable, in: ContentView.minimumTableOption...ContentView.maximumTableOption)
+                        
+                        Picker("Number of questions", selection: $numberOfQuestions) {
+                            ForEach(ContentView.numberOfQuestionsOptions, id: \.self) {
+                                Text("\($0) questions")
+                            }
                         }
                     }
                 }
-                
-                Section("Questions") {
-                    List(questions) { question in
-                        Text("\(question.firstNumber) x \(question.secondNumber) = \(question.answer)")
-                    }
+                .navigationTitle("Multiplication Tables")
+                .toolbar {
+                    Button("Start", action: start)
                 }
             }
-            .navigationTitle("Multiplication Tables")
-            .toolbar {
-                Button("Start", action: start)
+        } else {
+            VStack {
+                Spacer()
+                
+                Text(questions[currentQuestion].question)
+                
+                TextField("Answer", text: $answer)
+                
+                Button("Submit", action: submitAnswer)
+                
+                Spacer()
+            }
+            .alert(resultTitle, isPresented: $showingResult) {
+                Button("Next Question", action: nextQuestion)
+            } message: {
+                Text(resultMessage)
+            }
+            .alert("Game Over", isPresented: $showingFinalScore) {
+                Button("Play Again", action: restart)
+            } message: {
+                Text("You answered \(correctAnswers)/\(questions.count) correctly.")
             }
         }
     }
     
-    func start() {
+    private func start() {
         // Generate questions
         self.questions = (0..<numberOfQuestions).map { _ in
             let firstNumber = generateRandomNumber()
@@ -66,6 +102,9 @@ struct ContentView: View {
             
             return MultiplicationQuestion(firstNumber: firstNumber, secondNumber: secondNumber)
         }
+        
+        // Start the game
+        showingSettings = false
     }
     
     private func generateRandomNumber() -> Int {
@@ -73,6 +112,39 @@ struct ContentView: View {
         let upperBound = maximumTable
         
         return Int.random(in: lowerBound...upperBound)
+    }
+    
+    private func submitAnswer() {
+        let question = questions[currentQuestion]
+        
+        if Int(answer) == question.answer {
+            correctAnswers += 1
+            resultTitle = "Correct"
+        } else {
+            resultTitle = "Wrong"
+        }
+        
+        resultMessage = question.answerDescription
+        showingResult = true
+    }
+    
+    private func nextQuestion() {
+        if currentQuestion == questions.count - 1 {
+            showingFinalScore = true
+        } else {
+            answer = ""
+            currentQuestion += 1
+        }
+    }
+    
+    private func restart() {
+        showingSettings = true
+        correctAnswers = 0
+        questions = []
+        currentQuestion = 0
+        answer = ""
+        resultTitle = ""
+        resultMessage = ""
     }
 }
 
