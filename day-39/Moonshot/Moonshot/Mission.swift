327fc0220087087bcd6ecc6afe94430d9228ea79
Tyler Milner <tylermilner@gmail.com>
2023-08-27 17:19:19 -0500

    Day 40 - Using generics to load any kind of Codable data.

---
+++ day-39/Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Tyler Milner on 8/27/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
