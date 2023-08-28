557848310c466adfd47b1ff9c9225e99dcd336ac
Tyler Milner <tylermilner@gmail.com>
2023-08-27 21:52:51 -0500

    Day 42 - challenge 2 complete. Extracted crew scroll view into MissionCrewScrollView and custom divier into MoonshotDivider.

---
+++ day-39/Moonshot/Moonshot/MissionCrewScrollView.swift
@@ -0,0 +1,63 @@
+//
+//  MissionCrewScrollView.swift
+//  Moonshot
+//
+//  Created by Tyler Milner on 8/27/23.
+//
+
+import SwiftUI
+
+struct MissionCrewScrollView: View {
+    let crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew , id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(Capsule())
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                
+                                Text(crewMember.role)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MissionCrewScrollView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        let mission = missions[0]
+        let crew: [MissionView.CrewMember] = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return MissionView.CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+        
+        MissionCrewScrollView(crew: crew)
+    }
+}
--- day-39/Moonshot/Moonshot/MissionView.swift
+++ day-39/Moonshot/Moonshot/MissionView.swift
@@ -31,10 +31,7 @@ struct MissionView: View {
                         .font(.subheadline)
                     
                     VStack(alignment: .leading) {
-                        Rectangle()
-                            .frame(height: 2)
-                            .foregroundColor(.lightBackground)
-                            .padding(.vertical)
+                        MoonshotDivider()
                         
                         Text("Mission Highlights")
                             .font(.title.bold())
@@ -42,10 +39,7 @@ struct MissionView: View {
                         
                         Text(mission.description)
                         
-                        Rectangle()
-                            .frame(height: 2)
-                            .foregroundColor(.lightBackground)
-                            .padding(.vertical)
+                        MoonshotDivider()
                         
                         Text("Crew")
                             .font(.title.bold())
@@ -53,36 +47,7 @@ struct MissionView: View {
                     }
                     .padding(.horizontal)
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(crew , id: \.role) { crewMember in
-                                NavigationLink {
-                                    AstronautView(astronaut: crewMember.astronaut)
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width: 104, height: 72)
-                                            .clipShape(Capsule())
-                                            .overlay(
-                                                Capsule()
-                                                    .strokeBorder(.white, lineWidth: 1)
-                                            )
-                                        
-                                        VStack(alignment: .leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundColor(.white)
-                                                .font(.headline)
-                                            
-                                            Text(crewMember.role)
-                                                .foregroundColor(.secondary)
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                        }
-                    }
+                    MissionCrewScrollView(crew: crew)
                 }
                 .padding(.bottom)
             }
