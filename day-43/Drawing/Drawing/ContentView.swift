91998dbbadcf43ac62c1c0cf2583efeff6584621
Tyler Milner <tylermilner@gmail.com>
2023-09-08 22:17:33 -0500

    Day 44 complete.

--- day-43/Drawing/Drawing/ContentView.swift
+++ day-43/Drawing/Drawing/ContentView.swift
@@ -7,104 +7,109 @@
 
 import SwiftUI
 
-// Adding strokeBorder() support with InsettableShape
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
-
-    func path(in rect: CGRect) -> Path {
-        // SwiftUI Shapes measure their coordinates from the bottom left corner rather than the top left corner, causing the direction to be reversed
-        // Adjust parameters accordingly
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        var path = Path()
-
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-
-        return path
+// Enabling high-performance Metal rendering with drawingGroup()
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                            color(for: value, brightness: 1),
+                            color(for: value, brightness: 0.5)
+                        ]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ),
+                    lineWidth: 2
+                )
+            }
+        }
+        .drawingGroup()
     }
     
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
     var body: some View {
-//        Circle()
-//            .strokeBorder(.blue, lineWidth: 40)
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
-//// Paths vs shapes in SwiftUI
-//struct Triangle: Shape {
-//    func path(in rect: CGRect) -> Path {
-//        var path = Path()
-//
-//        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-//        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-//
-//        return path
-//    }
-//}
-//
-//struct Arc: Shape {
-//    let startAngle: Angle
-//    let endAngle: Angle
-//    let clockwise: Bool
-//
-//    func path(in rect: CGRect) -> Path {
-//        // SwiftUI Shapes measure their coordinates from the bottom left corner rather than the top left corner, causing the direction to be reversed
-//        // Adjust parameters accordingly
-//        let rotationAdjustment = Angle.degrees(90)
-//        let modifiedStart = startAngle - rotationAdjustment
-//        let modifiedEnd = endAngle - rotationAdjustment
-//
-//        var path = Path()
-//
-//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-//
-//        return path
-//    }
-//}
-//
+//// Creative borders and fills using ImagePaint
 //struct ContentView: View {
 //    var body: some View {
-////        Triangle()
-////            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+////        Text("Hello, world!")
 ////            .frame(width: 300, height: 300)
+////            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.4, width: 1, height: 0.2), scale: 0.1), width: 50)
 //
-//        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-//            .stroke(.blue, lineWidth: 10)
-//            .frame(width: 300, height: 300)
+//        Capsule()
+//            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+//            .frame(width: 300, height: 200)
 //    }
 //}
 
-//// Creating custom paths with SwiftUI
-//struct ContentView: View {
-//    var body: some View {
-//        Path { path in
-//            path.move(to: CGPoint(x: 200, y: 100))
-//            path.addLine(to: CGPoint(x: 100, y: 300))
-//            path.addLine(to: CGPoint(x: 300, y: 300))
-//            path.addLine(to: CGPoint(x: 200, y: 100))
-//            // Option 1 - close subpath to complete the final point
-////            path.closeSubpath()
+//// Transforming shapes using CGAffineTransform and even-odd fills
+//struct Flower: Shape {
+//    var petalOffset = -20.0
+//    var petalWidth = 100.0
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//
+//            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            path.addPath(rotatedPetal)
 //        }
-////        .fill(.blue)
-////        .stroke(.blue, lineWidth: 10)
-//        // Option 2 - use StrokeStyle to round off the final point
-//        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round,lineJoin: .round))
+//
+//        return path
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var petalOffset = -20.0
+//    @State private var petalWidth = 100.0
+//
+//    var body: some View {
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+////                .stroke(.red, lineWidth: 1)
+//                .fill(.red, style: FillStyle(eoFill: true))
+//
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40)
+//                .padding([.horizontal, .bottom])
+//
+//            Text("Width")
+//            Slider(value: $petalWidth, in: 0...100)
+//                .padding(.horizontal)
+//        }
 //    }
 //}
 
