97670f3b7c628cbe139a46aa68af824d30b6bcfb
Tyler Milner <tylermilner@gmail.com>
2023-09-09 14:30:54 -0500

    Day 45 complete.

--- day-43/Drawing/Drawing/ContentView.swift
+++ day-43/Drawing/Drawing/ContentView.swift
@@ -7,85 +7,131 @@
 
 import SwiftUI
 
-// Enabling high-performance Metal rendering with drawingGroup()
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+// Creating a spirograph with SwiftUI
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
     
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                            color(for: value, brightness: 1),
-                            color(for: value, brightness: 0.5)
-                        ]),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    ),
-                    lineWidth: 2
-                )
-            }
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
         }
-        .drawingGroup()
+        
+        return a
     }
     
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
         
-        if targetHue > 1 {
-            targetHue -= 1
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
         }
         
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        return path
     }
 }
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
     
     var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: colorCycle)
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
                 .frame(width: 300, height: 300)
             
-            Slider(value: $colorCycle)
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+                
+            }
         }
     }
 }
 
-//// Creative borders and fills using ImagePaint
-//struct ContentView: View {
-//    var body: some View {
-////        Text("Hello, world!")
-////            .frame(width: 300, height: 300)
-////            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.4, width: 1, height: 0.2), scale: 0.1), width: 50)
+//// Animating complex shapes with AnimatablePair
+//struct Checkerboard: Shape {
+//    var rows: Int
+//    var columns: Int
 //
-//        Capsule()
-//            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
-//            .frame(width: 300, height: 200)
+//    var animatableData: AnimatablePair<Double, Double> {
+//        get {
+//            AnimatablePair(Double(rows), Double(columns))
+//        }
+//
+//        set {
+//            rows = Int(newValue.first)
+//            columns = Int(newValue.second)
+//        }
 //    }
-//}
-
-//// Transforming shapes using CGAffineTransform and even-odd fills
-//struct Flower: Shape {
-//    var petalOffset = -20.0
-//    var petalWidth = 100.0
 //
 //    func path(in rect: CGRect) -> Path {
 //        var path = Path()
 //
-//        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-//            let rotation = CGAffineTransform(rotationAngle: number)
-//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//        let rowSize = rect.height / Double(rows)
+//        let columnSize = rect.width / Double(columns)
 //
-//            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-//            let rotatedPetal = originalPetal.applying(position)
+//        for row in 0..<rows {
+//            for column in 0..<columns {
+//                if (row + column).isMultiple(of: 2) {
+//                    let startX = columnSize * Double(column)
+//                    let startY = rowSize * Double(row)
 //
-//            path.addPath(rotatedPetal)
+//                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+//                    path.addRect(rect)
+//                }
+//            }
 //        }
 //
 //        return path
@@ -93,23 +139,107 @@ struct ContentView: View {
 //}
 //
 //struct ContentView: View {
-//    @State private var petalOffset = -20.0
-//    @State private var petalWidth = 100.0
+//    @State private var rows = 4
+//    @State private var columns = 4
 //
 //    var body: some View {
+//        Checkerboard(rows: rows, columns: columns)
+//            .onTapGesture {
+//                withAnimation(.linear(duration: 3)) {
+//                    rows = 8
+//                    columns = 16
+//                }
+//            }
+//    }
+//}
+
+//// Animating simple shapes with animatableData
+//struct Trapezoid: Shape {
+//    var insetAmount: Double
+//
+//    var animatableData: Double {
+//        get { insetAmount }
+//        set { insetAmount = newValue }
+//    }
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        path.move(to: CGPoint(x: 0, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+//
+//        return path
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var insetAmount = 50.0
+//
+//    var body: some View {
+//        Trapezoid(insetAmount: insetAmount)
+//            .frame(width: 200, height: 200)
+//            .onTapGesture {
+//                withAnimation {
+//                    insetAmount = Double.random(in: 10...90)
+//                }
+//            }
+//    }
+//}
+
+//// Special effects in SwiftUI: blurs, blending, and more
+//struct ContentView: View {
+//    @State private var amount = 0.0
+//
+//    var body: some View {
+////        ZStack {
+////            Image("PaulHudson")
+////
+////            Rectangle()
+////                .fill(.red)
+////                .blendMode(.multiply)
+////        }
+//
+//        // Alternative to ZStack - use `.colorMultiply()` modifier
+////        Image("PaulHudson")
+////            .colorMultiply(.red)
+//
 //        VStack {
-//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-////                .stroke(.red, lineWidth: 1)
-//                .fill(.red, style: FillStyle(eoFill: true))
+////            ZStack {
+////                Circle()
+////                    .fill(Color(red: 1, green: 0, blue: 0))
+////                    .frame(width: 200 * amount)
+////                    .offset(x: -50, y: -80)
+////                    .blendMode(.screen)
+////
+////                Circle()
+////                    .fill(Color(red: 0, green: 1, blue: 0))
+////                    .frame(width: 200 * amount)
+////                    .offset(x: 50, y: -80)
+////                    .blendMode(.screen)
+////
+////                Circle()
+////                    .fill(Color(red: 0, green: 0, blue: 1))
+////                    .frame(width: 200 * amount)
+////                    .blendMode(.screen)
+////            }
+////            .frame(width: 300, height: 300)
 //
-//            Text("Offset")
-//            Slider(value: $petalOffset, in: -40...40)
-//                .padding([.horizontal, .bottom])
+//            Image("PaulHudson")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 100, height: 200)
+//                .saturation(amount)
+//                .blur(radius: (1 - amount) * 20)
 //
-//            Text("Width")
-//            Slider(value: $petalWidth, in: 0...100)
-//                .padding(.horizontal)
+//            Slider(value: $amount)
+//                .padding()
 //        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.black)
+//        .ignoresSafeArea()
 //    }
 //}
 
