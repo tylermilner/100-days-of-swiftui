0eab6f2f99de24044a7c2d944bb925957763021a
Tyler Milner <tylermilner@gmail.com>
2023-09-09 19:42:37 -0500

    Day 46 - challenge 2 complete. Animate arrow line width.

--- day-43/Drawing/Drawing/ContentView.swift
+++ day-43/Drawing/Drawing/ContentView.swift
@@ -7,43 +7,62 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Arrow: InsettableShape {
+    var insetAmount = 0.0
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    // Draw the arrow
+    // Take into account `insetAmount` so that the arrow continues to fit inside of the rect when the line width changes
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        let triangleHeight = (rect.height / 3)
+        let baseWidth = max(1, floor(rect.width / 10))
+        
+        let baseMinX = rect.midX - (baseWidth / 2) + (insetAmount / 2)
+        let baseMaxX = rect.midX + (baseWidth / 2) - (insetAmount / 2)
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY + insetAmount))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: triangleHeight - insetAmount))
+        path.addLine(to: CGPoint(x: baseMaxX, y: triangleHeight - insetAmount))
+        path.addLine(to: CGPoint(x: baseMaxX, y: rect.maxY - insetAmount))
+        path.addLine(to: CGPoint(x: baseMinX, y: rect.maxY - insetAmount))
+        path.addLine(to: CGPoint(x: baseMinX, y: triangleHeight - insetAmount))
+        path.addLine(to: CGPoint(x: rect.minX + insetAmount, y: triangleHeight - insetAmount))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY + insetAmount))
         
         return path
     }
-}
-
-struct Arrow: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        let triangleRect = CGRect(x: rect.minX, y: rect.minY, width: rect.width, height: rect.height / 3)
-        let triangle = Triangle()
-        let trianglePath = triangle.path(in: triangleRect)
-        path.addPath(trianglePath)
-        
-        let thickness = max(1, floor(rect.width / 10))
-        let arrowMinX = triangleRect.midX - (thickness / 2)
-        let arrowMaxX = triangleRect.midX + (thickness / 2)
-        path.move(to: CGPoint(x: arrowMinX, y: triangleRect.maxY))
-        path.addLine(to: CGPoint(x: arrowMinX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: arrowMaxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: arrowMaxX, y: triangleRect.maxY))
-        
-        return path
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arrow = self
+        arrow.insetAmount += amount
+        return arrow
     }
 }
 
 struct ContentView: View {
+    @State private var arrowLineWidth = 2.0
+    
     var body: some View {
-        Arrow()
+        VStack {
+            Spacer()
+            
+            Arrow()
+                .strokeBorder(.blue, style: StrokeStyle(lineWidth: arrowLineWidth, lineCap: .round,lineJoin: .round))
+            
+            Button("Animate Line Width") {
+                withAnimation {
+                    arrowLineWidth = Double.random(in: 2...20)
+                }
+            }
+            
+            Spacer()
+        }
     }
 }
 
