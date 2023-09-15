5100f4e18302b9ba7288f8560a4287b06b7700ae
Tyler Milner <tylermilner@gmail.com>
2023-09-14 21:39:07 -0500

    Day 50 complete. Preparing for checkout.

--- day-49/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ day-49/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,7 +11,26 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233) // Reserve known image height ahead of time to keep height from jumping around
+                
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: { })
+                    .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
