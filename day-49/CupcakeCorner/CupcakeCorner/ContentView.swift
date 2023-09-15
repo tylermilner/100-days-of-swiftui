9c9e7bdffddf492c0cc136ab6d9b4febf98d30c3
Tyler Milner <tylermilner@gmail.com>
2023-09-14 20:58:59 -0500

    Day 50 WIP - Taking basic order details.

--- day-49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ day-49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,139 +7,43 @@
 
 import SwiftUI
 
-// Validating and disabling forms
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
+    @StateObject var order = Order()
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account...")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: {
+                        AddressView(order: order)
+                    }) {
+                        Text("Delivery details")
+                    }
                 }
             }
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
         }
     }
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
 }
-
-//// Loading an image from a remote server
-//struct ContentView: View {
-//    var body: some View {
-//        // Provide scale to render image at that exact scale
-////        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
-//
-//        // Apply modifiers to the image when downloaded
-////        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-////            image
-////                .resizable()
-////                .scaledToFit()
-////        } placeholder: {
-////            ProgressView()
-////        }
-////        .frame(width: 200, height: 200)
-//
-//        // Observe async image phases
-//        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-//            if let image = phase.image {
-//                image
-//                    .resizable()
-//                    .scaledToFit()
-//            } else if phase.error != nil {
-//                Text("There was an error loading the image.")
-//            } else {
-//                ProgressView()
-//            }
-//        }
-//    }
-//}
-
-//// Sending and receiving Codable data with URLSession and SwiftUI
-//struct Response: Codable {
-//    var results: [Result]
-//}
-//
-//struct Result: Codable {
-//    var trackId: Int
-//    var trackName: String
-//    var collectionName: String
-//}
-//
-//struct ContentView: View {
-//    @State private var results = [Result]()
-//
-//    var body: some View {
-//        List(results, id: \.trackId) { item in
-//            VStack {
-//                Text(item.trackName)
-//                    .font(.headline)
-//                Text(item.collectionName)
-//            }
-//        }
-//        .task {
-//            await loadData()
-//        }
-//    }
-//
-//    func loadData() async {
-//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-//            print("Invalid URL")
-//            return
-//        }
-//
-//        do {
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//
-//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-//                results = decodedResponse.results
-//            }
-//        } catch {
-//            print("Invalid data")
-//        }
-//    }
-//}
-
-//// Adding Codable conformance for @Published properties
-//class User: ObservableObject, Codable {
-//    enum CodingKeys: CodingKey {
-//        case name
-//    }
-//
-//    @Published var name = "Paul Hudson"
-//
-//    // Alternatively mark class as 'final' to remove 'required' keyword
-//    required init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        name = try container.decode(String.self, forKey: .name)
-//    }
-//
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(name, forKey: .name)
-//    }
-//}
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ day-49/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Tyler Milner on 9/14/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
