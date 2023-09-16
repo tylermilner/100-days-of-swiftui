31770e409ac41b9a36b88253b3c7836cee94b291
Tyler Milner <tylermilner@gmail.com>
2023-09-16 13:07:26 -0500

    Day 55 WIP. Showing book details.

---
+++ day-53/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,50 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Tyler Milner on 9/16/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No Review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// Apparently Previews are too much of a hastle to use with Core Data...
+//struct DetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailView()
+//    }
+//}
