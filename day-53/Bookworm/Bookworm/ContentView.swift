a8807a5d719577eec174408c5ef39d878835370e
Tyler Milner <tylermilner@gmail.com>
2023-09-16 12:20:27 -0500

    Day 54 WIP. Creating books with Core Data.

--- day-53/Bookworm/Bookworm/ContentView.swift
+++ day-53/Bookworm/Bookworm/ContentView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("")
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
     }
 }
 
