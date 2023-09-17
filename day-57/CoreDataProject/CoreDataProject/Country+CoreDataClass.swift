ae4227ee8c29c2805bf9210dd5c45838098c13cf
Tyler Milner <tylermilner@gmail.com>
2023-09-17 18:31:00 -0500

    Day 58 complete.

---
+++ day-57/CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Tyler Milner on 9/17/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
---
+++ day-57/CoreDataProject/CoreDataProject/FilteredList.swift
@@ -0,0 +1,33 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Tyler Milner on 9/17/23.
+//
+
+import CoreData
+import SwiftUI
+
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+    let content: (T) -> Content
+    
+    var body: some View {
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
+        }
+        
+    }
+    
+    // Use @ViewBuilder to allow multiple views to be provided as 'content'
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
+    }
+}
+
+//struct FilteredList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FilteredList()
+//    }
+//}
