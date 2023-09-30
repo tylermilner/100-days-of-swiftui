ae52a6fa2b55ddbce843cbbd3eaeab1658ae50e6
Tyler Milner <tylermilner@gmail.com>
2023-09-30 12:17:43 -0500

    Day 63 complete.

--- day-62/Instafilter/Instafilter/ContentView.swift
+++ day-62/Instafilter/Instafilter/ContentView.swift
@@ -5,30 +5,114 @@
 //  Created by Tyler Milner on 9/29/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-// Showing multiple options with confirmationDialog()
+// Wrapping a UIViewController in a SwiftUI view
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var showingImagePicker = false
     
     var body: some View {
-        Text("Hello, world!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
             }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+        }
+        .sheet(isPresented: $showingImagePicker, content: {
+            ImagePicker()
+        })
     }
 }
+
+//// Integrating Core Image with SwiftUI
+//struct ContentView: View {
+//    @State private var image: Image?
+//    
+//    var body: some View {
+//        VStack {
+//            image?
+//                .resizable()
+//                .scaledToFit()
+//        }
+//        .onAppear(perform: loadImage)
+//    }
+//    
+//    func loadImage() {
+//        guard let inputImage = UIImage(named: "Example") else { return }
+//        let beginImage = CIImage(image: inputImage)
+//        
+//        let context = CIContext()
+//        // Sepia filter
+////        let currentFilter = CIFilter.sepiaTone()
+////        currentFilter.inputImage = beginImage
+////        currentFilter.intensity = 1
+//        
+//        // Pixellate filter
+////        let currentFilter = CIFilter.pixellate()
+////        currentFilter.inputImage = beginImage
+////        currentFilter.scale = 100
+//        
+//        // Crystallize filter
+////        let currentFilter = CIFilter.crystallize()
+////        currentFilter.inputImage = beginImage
+////        currentFilter.radius = 100
+//        
+//        // Twirl distortion filter
+//        let currentFilter = CIFilter.twirlDistortion()
+//        currentFilter.inputImage = beginImage
+//        
+//        let amount = 1.0
+//        let inputKeys = currentFilter.inputKeys
+//        
+//        if inputKeys.contains(kCIInputIntensityKey) {
+//            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+//        }
+//        
+//        if inputKeys.contains(kCIInputRadiusKey) {
+//            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+//        }
+//        
+//        if inputKeys.contains(kCIInputScaleKey) {
+//            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+//        }
+//        
+//        guard let outputImage = currentFilter.outputImage else { return }
+//        
+//        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+//            let uiImage = UIImage(cgImage: cgimg)
+//            image = Image(uiImage: uiImage)
+//        }
+//    }
+//}
+
+//// Showing multiple options with confirmationDialog()
+//struct ContentView: View {
+//    @State private var showingConfirmation = false
+//    @State private var backgroundColor = Color.white
+//    
+//    var body: some View {
+//        Text("Hello, world!")
+//            .frame(width: 300, height: 300)
+//            .background(backgroundColor)
+//            .onTapGesture {
+//                showingConfirmation = true
+//            }
+//            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+//                Button("Red") { backgroundColor = .red }
+//                Button("Green") { backgroundColor = .green }
+//                Button("Blue") { backgroundColor = .blue }
+//                Button("Cancel", role: .cancel) { }
+//            } message: {
+//                Text("Select a new color")
+//            }
+//    }
+//}
 
 //// Responding to state changes using onChange()
 //struct ContentView: View {
