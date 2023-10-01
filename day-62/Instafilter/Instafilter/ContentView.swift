be0276bb528f3a3014c01b6813c1d4e5f59a9f52
Tyler Milner <tylermilner@gmail.com>
2023-10-01 14:50:01 -0500

    Day 67 - challenge 1 complete. Disable save button if no image is currently selected.

--- day-62/Instafilter/Instafilter/ContentView.swift
+++ day-62/Instafilter/Instafilter/ContentView.swift
@@ -56,6 +56,7 @@ struct ContentView: View {
                     Spacer()
                     
                     Button("Save", action: save)
+                        .disabled(image == nil)
                 }
             }
             .padding([.horizontal, .bottom])
