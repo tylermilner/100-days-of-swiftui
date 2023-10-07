d1345b829d86e82bba8bcf48a8a315780fdd7682
Tyler Milner <tylermilner@gmail.com>
2023-10-06 21:14:06 -0500

    Day 69 complete.

--- day-68/BucketList/BucketList/ContentView.swift
+++ day-68/BucketList/BucketList/ContentView.swift
@@ -5,45 +5,115 @@
 //  Created by Tyler Milner on 10/5/23.
 //
 
+import LocalAuthentication
+import MapKit
 import SwiftUI
 
-// Switching view states with enums
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
+// Using Touch ID and Face ID with SwiftUI
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var isUnlocked = false
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
+
+//// Integrating MapKit with SwiftUI
+//struct Location: Identifiable {
+//    let id = UUID()
+//    let name: String
+//    let coordinate: CLLocationCoordinate2D
+//}
+//
+//struct ContentView: View {
+//    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+//    
+//    let locations = [
+//        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+//        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+//    ]
+//    
+//    var body: some View {
+//        NavigationView {
+//            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+//                MapAnnotation(coordinate: location.coordinate) {
+//                    NavigationLink {
+//                        Text(location.name)
+//                    } label: {
+//                        Circle()
+//                            .stroke(.red, lineWidth: 3)
+//                            .frame(width: 44, height: 44)
+//                    }
+//                }
+//            }
+//            .navigationTitle("London Explorer")
+//        }
+//    }
+//}
+
+//// Switching view states with enums
+//enum LoadingState {
+//    case loading, success, failed
+//}
+//
+//struct LoadingView: View {
+//    var body: some View {
+//        Text("Loading...")
+//    }
+//}
+//
+//struct SuccessView: View {
+//    var body: some View {
+//        Text("Success!")
+//    }
+//}
+//
+//struct FailedView: View {
+//    var body: some View {
+//        Text("Failed.")
+//    }
+//}
+//
+//struct ContentView: View {
+//    var loadingState = LoadingState.loading
+//    
+//    var body: some View {
+//        switch loadingState {
+//        case .loading:
+//            LoadingView()
+//        case .success:
+//            SuccessView()
+//        case .failed:
+//            FailedView()
+//        }
+//    }
+//}
 
 //// Writing data to the documents directory
 //struct ContentView: View {
