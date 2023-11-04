e8aa96959ceb974e2bca12f5a6f8384006c0e7bc
Tyler Milner <tylermilner@gmail.com>
2023-11-04 16:17:26 -0500

    Day 77 WIP. Reorganize UX flow to better match challenge description. Show the image picker directly and then ask for the name in an alert instead of showing a full Add Friend sheet.

--- day-77/Confriends/Confriends/ContentView.swift
+++ day-77/Confriends/Confriends/ContentView.swift
@@ -8,43 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAddFriend = false
+    @State private var showingAddFriendAlert = false
+    @State private var name: String = ""
     
-    // TODO: Remove test data
-    @State private var friends = [
-        Friend(name: "One"),
-        Friend(name: "Two"),
-        Friend(name: "Three")
-    ].sorted()
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    @State private var friends: [Friend] = [].sorted()
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(friends) { friend in
-                    NavigationLink {
-                        FriendDetailView(friend: friend)
-                    } label: {
-                        Text(friend.name)
+            Group {
+                if friends.isEmpty {
+                    Text("No friends yet 😔")
+                } else {
+                    List {
+                        ForEach(friends) { friend in
+                            NavigationLink {
+                                FriendDetailView(friend: friend)
+                            } label: {
+                                Text(friend.name)
+                            }
+                        }
+                        .onDelete(perform: removeItems)
                     }
                 }
-                .onDelete(perform: removeItems)
             }
             .navigationTitle("Confriends")
             .toolbar {
                 Button {
-                    showingAddFriend = true
+                    showingImagePicker = true
                 } label: {
                     Image(systemName: "plus")
                 }
             }
-            .sheet(isPresented: $showingAddFriend) {
-                AddFriendView()
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
             }
+            .alert("Add Friend", isPresented: $showingAddFriendAlert) {
+                TextField("Name", text: $name)
+                
+                Button("Cancel", role: .cancel) {
+                    resetValues()
+                }
+                Button("Save") {
+                    saveFriend()
+                }
+            } message: {
+                Text("Enter friend's name")
+            }
+            .onChange(of: inputImage, perform: imageSelected)
         }
     }
 }
 
 private extension ContentView {
+    func imageSelected(_ image: UIImage?) {
+        guard let image = image else { return }
+        showingAddFriendAlert = true
+    }
+    
+    func saveFriend() {
+        let friend = Friend(name: name)
+        
+        // TODO: Save friend to persistent storage
+        friends.append(friend)
+        
+        resetValues()
+    }
+    
+    func resetValues() {
+        name = ""
+        inputImage = nil
+    }
+    
     func removeItems(at offsets: IndexSet) {
         friends.remove(atOffsets: offsets)
         // TODO: Remove friend from persistent storage
