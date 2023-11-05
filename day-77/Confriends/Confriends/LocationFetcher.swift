c3989d02c1a32dfb174d480905adffda56aa955e
Tyler Milner <tylermilner@gmail.com>
2023-11-05 17:10:09 -0600

    Day 78 WIP. Add LocationFetcher helper and "Privacy - Location When In Use Usage Description" value to Info.plist.

---
+++ day-77/Confriends/Confriends/LocationFetcher.swift
@@ -0,0 +1,28 @@
+//
+//  LocationFetcher.swift
+//  Confriends
+//
+//  Created by Tyler Milner on 11/5/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
