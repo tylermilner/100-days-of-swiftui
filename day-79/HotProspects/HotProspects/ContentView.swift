4dd564e0c99728213574fc4afd8fc080d9eecd1e
Tyler Milner <tylermilner@gmail.com>
2023-11-07 22:50:39 -0600

    Day 81 complete.

--- day-79/HotProspects/HotProspects/ContentView.swift
+++ day-79/HotProspects/HotProspects/ContentView.swift
@@ -5,20 +5,125 @@
 //  Created by Tyler Milner on 11/7/23.
 //
 
+import SamplePackage
 import SwiftUI
+import UserNotifications
 
-// Controlling image interpolation in SwiftUI
+// Adding Swift package dependencies in Xcode
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
+    
     var body: some View {
-        Image("example")
-            .interpolation(.none) // removes the pixel "blurring" that iOS automatically applies when scaling images up
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
+        Text(results)
+    }
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
     }
 }
+
+//// Scheduling local notifications
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Button("Request Permission") {
+//                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+//                    if success {
+//                        print("All set!")
+//                    } else if let error = error {
+//                        print(error.localizedDescription)
+//                    }
+//                }
+//            }
+//            
+//            Button("Schedule Notification") {
+//                let content = UNMutableNotificationContent()
+//                content.title = "Feed the dogs"
+//                content.subtitle = "They look hungry"
+//                content.sound = UNNotificationSound.default
+//                
+//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//                
+//                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+//                
+//                UNUserNotificationCenter.current().add(request)
+//            }
+//        }
+//    }
+//}
+
+//// Adding custom row swipe animations to a List
+//struct ContentView: View {
+//    var body: some View {
+//        List {
+//            Text("Taylor Swift")
+//                .swipeActions {
+//                    Button(role: .destructive) {
+//                        print("Deleting")
+//                    } label: {
+//                        Label("Delete", systemImage: "minus.circle")
+//                    }
+//                }
+//                .swipeActions(edge: .leading) {
+//                    Button {
+//                        print("Pinning")
+//                    } label: {
+//                        Label("Pin", systemImage: "pin")
+//                    }
+//                    .tint(.orange)
+//                }
+//        }
+//    }
+//}
+
+//// Creating context menus
+//struct ContentView: View {
+//    @State private var backgroundColor = Color.red
+//    
+//    var body: some View {
+//        VStack {
+//            Text("Hello, world!")
+//                .padding()
+//                .background(backgroundColor)
+//            
+//            Text("Change Color")
+//                .padding()
+//                .contextMenu {
+//                    // Set button role to destructive to make it red
+//                    Button(role: .destructive) {
+//                        backgroundColor = .red
+//                    } label: {
+//                        Label("Red", systemImage: "checkmark.circle.fill")
+//                        // Setting foreground color is ignored in a context menu
+////                            .foregroundColor(.red)
+//                    }
+//                    
+//                    Button("Green") {
+//                        backgroundColor = .green
+//                    }
+//                    
+//                    Button("Blue") {
+//                        backgroundColor = .blue
+//                    }
+//                }
+//        }
+//    }
+//}
+
+//// Controlling image interpolation in SwiftUI
+//struct ContentView: View {
+//    var body: some View {
+//        Image("example")
+//            .interpolation(.none) // removes the pixel "blurring" that iOS automatically applies when scaling images up
+//            .resizable()
+//            .scaledToFit()
+//            .frame(maxHeight: .infinity)
+//            .background(.black)
+//            .ignoresSafeArea()
+//    }
+//}
 
 //// Understanding Swift's Result type
 //struct ContentView: View {
