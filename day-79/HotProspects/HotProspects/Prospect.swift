379d850bc5d2f35a7c5c68e32ce67b71f468ea72
Tyler Milner <tylermilner@gmail.com>
2023-11-09 22:08:53 -0600

    Day 84 complete.

--- day-79/HotProspects/HotProspects/Prospect.swift
+++ day-79/HotProspects/HotProspects/Prospect.swift
@@ -15,14 +15,35 @@ class Prospect: Identifiable, Codable {
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        // no saved data!
         people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.setValue(encoded, forKey: saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
     }
 }
