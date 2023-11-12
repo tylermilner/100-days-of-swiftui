ca88370498e4efd6b87394c018b4cce51706eef2
Tyler Milner <tylermilner@gmail.com>
2023-11-11 21:05:14 -0600

    Day 86 complete.

---
+++ day-86/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Tyler Milner on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
