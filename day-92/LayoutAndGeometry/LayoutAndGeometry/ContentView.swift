88aa38a5bb2f69bbccabd645571056faf2a39872
Tyler Milner <tylermilner@gmail.com>
2023-11-18 13:30:52 -0600

    Day 94 challenge 2 complete. Scale views down the closer they are to the top.

--- day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity(min(1, max(0, geo.frame(in: .global).minY / 200)))
+                            .scaleEffect(x: min(1, max(0.5, geo.frame(in: .global).minY / 200)), y: min(1, max(0.5, geo.frame(in: .global).minY / 200)))
                     }
                     .frame(height: 40)
                 }
