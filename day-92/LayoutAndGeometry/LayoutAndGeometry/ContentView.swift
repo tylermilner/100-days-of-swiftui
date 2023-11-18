c537928f476baed8cc010e9e70ace9489ad81253
Tyler Milner <tylermilner@gmail.com>
2023-11-18 13:19:50 -0600

    Day 94 challenge 1 complete. Fade views near top of scroll view to 0 opacity.

--- day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(min(1, max(0, geo.frame(in: .global).minY / 200)))
                     }
                     .frame(height: 40)
                 }
