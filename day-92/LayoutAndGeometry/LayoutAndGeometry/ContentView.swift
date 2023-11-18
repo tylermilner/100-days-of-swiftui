cf9350841370757b7c63bb2f375ccd3183f1ad03
Tyler Milner <tylermilner@gmail.com>
2023-11-18 00:24:03 -0600

    Day 93 complete.

--- day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ day-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,46 +7,159 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    // Not designed to be instantiated so better to make this an enum rather than a struct
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
+// ScrollView effects using GeometryReader
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    var body: some View {
+//        GeometryReader { fullView in
+//            ScrollView {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+//                            .rotation3DEffect(
+//                                .degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0)
+//                            )
+//                    }
+//                    .frame(minHeight: 40)
+//                }
+//            }
+//        }
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
         }
     }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
-struct ContentView: View {
-    var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("paul-hudson")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
-    }
-}
+//// Understanding frames and coordinates
+//struct OuterView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Top")
+//            InnerView()
+//                .background(.green)
+//            Text("Bottom")
+//        }
+//    }
+//}
+//
+//struct InnerView: View {
+//    var body: some View {
+//        HStack {
+//            Text("Left")
+//            
+//            GeometryReader { geo in
+//                Text("Center")
+//                    .background(.blue)
+//                    .onTapGesture {
+//                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+//                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+//                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+//                    }
+//            }
+//            .background(.orange)
+//            
+//            Text("Right")
+//        }
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+////        VStack {
+////            // GeometryReader automatically expands to fill the entire space
+////            GeometryReader { geo in
+////                Text("Hello, world!")
+////                    .frame(width: geo.size.width * 0.9)
+////                    .background(.red)
+////            }
+////            .background(.green)
+////            
+////            Text("More text")
+////            Text("More text")
+////            Text("More text")
+////            Text("More text")
+////            Text("More text")
+////                .background(.blue)
+////        }
+//        
+//        OuterView()
+//            .background(.red)
+//            .coordinateSpace(name: "Custom")
+//    }
+//}
+
+//// Absolute positioning for SwiftUI views
+//struct ContentView: View {
+//    var body: some View {
+////        Text("Hello, world!")
+////            // .position modifier takes up all available space so that it can ensure it can layout it's child (Text) properly
+////            // This causes the .background(red) to take up the full screen
+////            .position(x: 100, y: 100)
+////            .background(.red)
+//        
+//        Text("Hello, world!")
+//            .offset(x: 100, y: 100)
+//            .background(.red)
+//    }
+//}
+
+//extension VerticalAlignment {
+//    // Not designed to be instantiated so better to make this an enum rather than a struct
+//    enum MidAccountAndName: AlignmentID {
+//        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+//            context[.top]
+//        }
+//    }
+//    
+//    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        HStack(alignment: .midAccountAndName) {
+//            VStack {
+//                Text("@twostraws")
+//                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                Image("paul-hudson")
+//                    .resizable()
+//                    .frame(width: 64, height: 64)
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//            }
+//            
+//            VStack {
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("PAUL HUDSON")
+//                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                    .font(.largeTitle)
+//            }
+//        }
+//    }
+//}
 
 //// Alignment and alignment guides
 //struct ContentView: View {
