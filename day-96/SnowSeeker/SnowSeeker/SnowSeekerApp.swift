cd1082d5130cd2cf5c0dc948ecd64ab4f34d5842
Tyler Milner <tylermilner@gmail.com>
2023-11-20 23:56:06 -0600

    Day 96 complete.

---
+++ day-96/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Tyler Milner on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
