0c3de57ece2ddee244c1502c0d252dcd08b63cff
Tyler Milner <tylermilner@gmail.com>
2023-12-02 14:56:48 -0600

    Day 99 challenge 3 complete. Add sorting dialog to sort resorts by default, alphabetical, or country order. Day 99 complete.

--- day-96/SnowSeeker/SnowSeeker/ContentView.swift
+++ day-96/SnowSeeker/SnowSeeker/ContentView.swift
@@ -20,8 +20,15 @@ extension View {
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
+    enum FilterSort {
+        case `default`, alphabetical, country
+    }
+    
     @StateObject var favorites = Favorites()
     @State private var searchText = ""
+    
+    @State private var filterSort: FilterSort = .default
+    @State private var isShowingSortDialog = false
     
     var body: some View {
         NavigationView {
@@ -58,6 +65,26 @@ struct ContentView: View {
             }
             .navigationTitle("Resorts")
             .searchable(text: $searchText, prompt: "Search for a resort")
+            .toolbar {
+                Button {
+                    isShowingSortDialog = true
+                } label: {
+                    Label("Sort", systemImage: "arrow.up.arrow.down")
+                }
+            }
+            .confirmationDialog("Sort", isPresented: $isShowingSortDialog) {
+                Button("Default") {
+                    filterSort = .default
+                }
+                
+                Button("Alphabetical") {
+                    filterSort = .alphabetical
+                }
+                
+                Button("Country") {
+                    filterSort = .country
+                }
+            }
             
             WelcomeView()
         }
@@ -66,11 +93,22 @@ struct ContentView: View {
 //        .phoneOnlyNavigationView()
     }
     
+    var sortedResorts: [Resort] {
+        switch filterSort {
+        case .default:
+            return resorts
+        case .alphabetical:
+            return resorts.sorted { $0.name < $1.name }
+        case .country:
+            return resorts.sorted { $0.country < $1.country }
+        }
+    }
+    
     var filteredResorts: [Resort] {
         if searchText.isEmpty {
-            return resorts
+            return sortedResorts
         } else {
-            return resorts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+            return sortedResorts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
         }
     }
 }
